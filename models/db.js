c2f90e830ca9a58fba080f17c87d97d6bea1202b
haesooKr <gotn676@gmail.com>
2020-04-25 08:43:08 -0400

    Modified db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localchost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb://localchost:27017/EmployeeDB', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
   if(!err) { console.log('MongoDB Connection Succeeded.') }
   else { console.log('Error in DB connection : ' + err) }
 })
