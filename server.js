3623b71586aaaf55878ab6fe611e0ac93c7bd956
haesooKr <gotn676@gmail.com>
2020-04-25 08:42:56 -0400

    Add server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-require('./models/db');
+require('./models/db.js')
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const express = require('express');
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Express server is running on http://localhost:${PORT}`);
+})
