bf338860e23ec94458fb2e177fb012a73f93c485
haesooKr <gotn676@gmail.com>
2020-04-25 09:02:29 -0400

    Add view engine

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
-const employeeController = require('./controllers/employeeController')
+const employeeController = require('./controllers/employeeController');
 
 const app = express();
 
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
 
 app.listen(PORT, () => {
   console.log(`Express server is running on http://localhost:${PORT}`);
