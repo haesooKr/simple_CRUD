6ce84e24ee818648c7928fd928c933f5f2cf23f6
haesooKr <gotn676@gmail.com>
2020-04-24 23:01:22 -0400

    Connect MongoDB

---
+++ models/db.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localchost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+  if(!err) { console.log('MongoDB Connection Succeeded.') }
+  else { console.log('Error in DB connection : ' + err) }
+})
