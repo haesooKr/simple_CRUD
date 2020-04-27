54caced000042dd3a5a5ddf464ad2cff354328d4
haesooKr <gotn676@gmail.com>
2020-04-26 22:38:53 -0400

    Add style

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render("employee/addOrEdit", {
-    viewTitle : "Insert Employee"
+    viewTitle : "Insert Employee",
+    style: "addOrEdit"
   });
 })
 
