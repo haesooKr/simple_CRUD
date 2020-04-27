08eb52935c5e7e12c183e49fadaf937b20da602c
haesooKr <gotn676@gmail.com>
2020-04-26 23:25:57 -0400

    link bodyparser

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ require('./models/db.js')
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
 
 const employeeController = require('./controllers/employeeController');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 app.use(express.static("./public/css/"))
 app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
