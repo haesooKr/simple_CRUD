43baad2a29f7833603b6980d0b66c566158c2f79
haesooKr <gotn676@gmail.com>
2020-04-26 23:26:53 -0400

    Employee Insert & Validation

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,12 +1,61 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
+const Employee = mongoose.model('Employee')
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   res.render("employee/addOrEdit", {
-    viewTitle : "Insert Employee",
-    style: "addOrEdit"
+    viewTitle: "Insert Employee",
+    style: "addOrEdit",
   });
-})
+});
+
+router.post("/", (req, res) => {
+  insertRecord(req, res);
+});
+
+function insertRecord(req, res){
+  let employee = new Employee();
+  employee.fullName = req.body.fullName;
+  employee.email = req.body.email;
+  employee.mobile = req.body.mobile;
+  employee.city = req.body.city;
+  employee.save((err, doc) => {
+    if(!err){
+      res.redirect('employee/list');
+    }
+    else {
+      if(err.name == "ValidationError"){
+        handleValidationError(err, req.body);
+        res.render("employee/addOrEdit", {
+          viewTitle: "Insert Employee",
+          employee: req.body,
+          style: "addOrEdit"
+        });
+      } else {
+        console.log('Error during record insertion : ' + err)
+      }
+    }
+  })
+}
+
+router.get("/list", (req, res) => {
+  res.json('from list');
+});
+
+function handleValidationError(err, body){
+  for(field in err.errors){
+    switch(err.errors[field].path){
+      case 'fullName':
+        body['fullNameError'] = err.errors[field].message;
+        break;
+      case 'email':
+        body['emailError'] = err.errors[field].message;
+        break;
+      default:
+        break;
+    }
+  }
+}
 
 module.exports = router;
--- models/employee.model.js
+++ models/employee.model.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 let employeeSchema = new mongoose.Schema({
   fullName: {
-    type: String
+    type: String,
+    required: "This field is required."
   },
   email: {
     type: String
@@ -14,5 +15,10 @@ let employeeSchema = new mongoose.Schema({
     type: String
   },
 });
+
+employeeSchema.path('email').validate((email) => {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email)
+}, 'Invalid e-mail.')
 
 mongoose.model('Employee', employeeSchema);
