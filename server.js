4e4c826293bae8da33e5c5747c82fce17be5b929
haesooKr <gotn676@gmail.com>
2020-04-28 02:28:27 -0400

    Update static directory

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-app.use(express.static("./public/css/"))
+app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ 
   extname: 'hbs', 
